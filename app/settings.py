c4280ac92b072058ca464999f20eae1e2e6e3856
Oleksandr Akhobadze <sandoahobadze@gmail.com>
2025-02-17 14:47:22 +0100

    securing django_secret_key and debug is safe for usage

--- app/settings.py
+++ app/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qwm)xm9e!gwp=w6r7sa^xw4^v^l&ipu#fm4j2iti=a55ms_c9_'
+load_dotenv()
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = os.getenv("DJANGO_DEBUG", "False") != "False"
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
